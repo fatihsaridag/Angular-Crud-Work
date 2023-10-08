ac8820101e23082ebe7e7f6ef087cb664151b024
Fatih Sarıdağ <68101192+fatihsaridag@users.noreply.github.com>
2023-10-08 13:38:34 +0300

    Todo App

--- TodoApp/src/app/app.component.ts
+++ TodoApp/src/app/app.component.ts
@@ -10,29 +10,30 @@ import { Component } from '@angular/core';
                   <hr>
                   <div class="form-group">
                     <label for="work"></label>
-                    <input [(ngModel)]="work" type="text" class="form-control" id="work" name="work"/>
+                    <input [(ngModel)]="work" type="text" [class]="ChangeInputClass()"  id="work" name="work"/>
                   </div>
                   <div class="form-group mt-2">
-                    <div class="btn btn-outline-primary w-100" (click)="Kaydet()">Kaydet</div>
+                    <button class="btn btn-outline-primary w-100" (click)="Kaydet()" >Kaydet</button>
                   </div>
                   <hr>
 
                       <ul>
                         <li *ngFor="let work of workList let i = index">{{work}}
-                          <button class="btn btn-outline-warning btn-sm" (click)="Get(i)">Güncelle</button>
+                          <button class="btn btn-outline-warning btn-sm" (click)="Get(work,i)">Güncelle</button>
                           <button class="btn btn-outline-danger btn-sm" (click)="Remove(i)">Sil</button>
                         </li>
                       </ul>
   </div>
-  <ng-template #update>
+  <ng-template #update> 
   <h1>Todo App</h1>
                   <hr>
                   <div class="form-group">
                     <label for="work"></label>
-                    <input [(ngModel)]="updateWork" type="text" class="form-control" id="work" name="work"/>
+                    <input [(ngModel)]="updateWork" type="text" class="form-control" [ngClass]="ChangeInputClass()" id="work" name="work"/>
                   </div>
                   <div class="form-group mt-2">
-                    <div class="btn btn-outline-primary w-100" (click)="Kaydet()">Kaydet</div>
+                    <div class="btn btn-outline-warning w-100" (click)="Update()">Güncelle</div>
+                    <div class="btn btn-outline-danger w-100" (click)="Cancel()">İptal</div>
                   </div>
                   <hr>
 
@@ -70,7 +71,16 @@ export class AppComponent {
   }
 
 
-  Get(index : number ) {
+
+  ChangeInputClass(){
+    if(this.work.length < 3 )
+      return("form-control is-invalid")
+    return("form-control is-valid")
+  }
+
+  Get(work:string, index : number ) {
+    debugger;
+    this.updateWork = work;
     this.isUpdateFormActive = true;
     this.index = index;
   }
